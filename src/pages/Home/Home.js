984a65426c363586a5777afbba8ec9249864d535
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2023-11-22 20:50:55 +0600

    our features section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,12 +2,15 @@ import React from 'react'
 import './Home.css'
 import HomeCarousel from '../../components/Home/HomeCarousel'
 import AboutUs from '../../components/Home/AboutUs'
+import OurFeatures from '../../components/Home/OurFeatures'
 
 const Home = () => {
     return (
         <div>
             <HomeCarousel />
             <AboutUs />
+            <hr />
+            <OurFeatures />
         </div>
     )
 }
