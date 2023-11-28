c519cd94834930e5457e237f9173a909d515aacc
Amit Mahmud Sabbir <amitsclan26@gmail.com>
2023-11-28 23:40:37 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import './App.css';
 
-import SharedNav from './components/Shared/SharedNav/SharedNav';
-import Footer from './components/Shared/Footer/Footer';
-import { Outlet } from 'react-router-dom';
 import Home from './pages/Home/Home';
 
 function App() {
