dec70947c7ad887d9aab11d33a82f92a19d9a69c
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2023-12-04 01:36:15 +0600

    doctor details modal

--- src/components/Services/SingleSpecialistCard/SingleSpecialistCard.js
+++ src/components/Services/SingleSpecialistCard/SingleSpecialistCard.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import { Button, Card } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Card, Modal } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const SingleSpecialistCard = ({ specialist }) => {
+    const [showDoctorDetails, setShowDoctorDetails] = useState(false);
 
+    // Function to show the dr. details modal
+    const handleShowDoctorDetails = () => {
+        setShowDoctorDetails(true);
+    };
+
+    // Function to hide the dr. details modal
+    const handleCloseDoctorDetails = () => {
+        setShowDoctorDetails(false);
+    };
+    console.log(specialist)
 
     return (
         <Card style={{ width: '300px', height: '400px', borderRadius: '15px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
@@ -17,7 +28,26 @@ const SingleSpecialistCard = ({ specialist }) => {
                     <Card.Title style={{ fontSize: '1.2rem', fontWeight: 'bold' }}>Dr. {specialist?.firstname + " " + specialist.lastname}</Card.Title>
                     <Card.Text style={{ textAlign: 'justify', color: '#555' }}>Degrees: {specialist?.company?.bs}</Card.Text>
                 </div>
-                <Button variant="outline-primary" style={{ alignSelf: 'flex-end' }}>Details</Button>
+                <Button onClick={handleShowDoctorDetails} variant="outline-primary" style={{ alignSelf: 'flex-end' }}>Details</Button>
+                <Modal show={showDoctorDetails} onHide={handleCloseDoctorDetails}>
+                    <Modal.Header closeLabel='cancel'>
+                        <Modal.Title>Dr. {specialist?.firstname + " " + specialist.lastname}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div>
+                            <p>Degrees: {specialist?.company?.bs}</p>
+                            <p>Address: {specialist?.address.street}, {specialist?.address.city}</p>
+                            <p>Email: {specialist.email}</p>
+                            <p>Phone: {specialist.phone}</p>
+                            <p>Website: {specialist.website}</p>
+                        </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleCloseDoctorDetails}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
                 <Button variant="outline-secondary" style={{ alignSelf: 'flex-end' }}>Take Appointment</Button>
             </Card.Body>
         </Card>
--- src/pages/ServicesPage/SpecialistsPage/SpecialistsPage.js
+++ src/pages/ServicesPage/SpecialistsPage/SpecialistsPage.js
@@ -38,13 +38,14 @@ const SpecialistsPage = () => {
 
 
 
+
   return (
     <div className='container mt-5'>
       <h2>Service Name:  {service?.slug}</h2>
       <div className='centering_items_flex'>
         <img width={375} height={300} src="https://img.freepik.com/free-vector/tiny-veterinarians-examining-dog-flat-vector-illustration-doctor-treating-huge-animal-vet-clinic-taking-care-health-determining-diagnosis-hospital-medical-pet-service-veterinary-concept_74855-25368.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699660800&semt=ais" alt="hello" />
 
-        <p className='text-justify' style={{height:'50%', width:'60%'}}>{service?.content}</p>
+        <p className='text-justify' style={{ height: '50%', width: '60%' }}>{service?.content}</p>
       </div>
       <div className=' mt-5'>
         <h3>Specialist for {service?.slug}</h3>
