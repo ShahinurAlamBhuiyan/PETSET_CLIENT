89fcbb183e65c549a9b203d09c7477c83d2d9a16
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2023-12-16 17:11:34 +0600

    home page done

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,18 +5,16 @@ import AboutUs from '../../components/Home/AboutUs'
 import WhyChooseUs from '../../components/Home/WhyChooseUs'
 import PopularSpecialist from '../../components/Home/PopularSpecialist'
 import OurFeatures from '../../components/Home/OurFeatures'
-import FAQ from '../../components/Home/FAQ'
 
 const Home = () => {
     return (
-        <div>
+        <>
             <HomeCarousel />
             <AboutUs />
             <OurFeatures />
             <WhyChooseUs />
             <PopularSpecialist />
-            <FAQ />
-        </div>
+        </>
     )
 }
 
