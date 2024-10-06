317f753b5ddaa7824832eaf069d2df8059114578
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-10-07 03:43:09 +0600

    Finishing touch

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home/Home';
 
 function App() {
-  console.log('Hello World');
   return (
     <div className='memory_amit_container'>
       <Home />
